a0eb2948573c506863b3b3c0a2d96e3d8e8df65f
Rohit Jha <rohitj.ug19.cs@nitp.ac.in>
2022-04-18 13:31:29 +0530

    Connected to blockchain and displayed the files of user.

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,79 @@
 import React, { Component } from 'react';
 import NITP from '../NITP.png';
 import './App.css';
-
+import Web3 from 'web3';
+import file from '../abis/File.json'
 
 //For file upload using ipfs
 const ipfsClient = require('ipfs-http-client')
-const ipfs = ipfsClient({host: "ipfs.infura.io", port:5001,  protocol: 'https'})
+const ipfs = ipfsClient({ host: "ipfs.infura.io", port: 5001, protocol: 'https' })
 
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+
+  //get account
+  //get network
+  //get smart contract
+  //----> ABI: file.abi
+  //----> Address: networkData.address
+  //get file hash
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    //get network id
+    const networkId = await web3.eth.net.getId()
+
+    const networkData = file.networks[networkId]
+    if (networkData) {
+      const abi = file.abi
+      const address = networkData.address
+      //Fetch contract
+      const contract = web3.eth.Contract(abi, address)
+      this.setState({ contract: contract })
+
+      //call getInfo method of contract
+      const record = await contract.methods.getInfo(0).call()
+      console.log(record)
+      this.setState({ record: record })
+    }
+    else {
+      window.alert('Smart contract not deployed to detected network !')
+    }
+  }
+
+
   constructor(props) {
     super(props);
     this.state = {
-      account:"",
+      account: "",
       buffer: null,
       contract: null,
-      record: null,
-      type:"aadhar",
-      fileHash:"QmdE3yFxHLY4gN5U8HDRnpX3Nj4UMrBkrsdnGWsgjLEyJ4"
+      record: {},
+      type: "aadhar",
+      fileHash: "QmdE3yFxHLY4gN5U8HDRnpX3Nj4UMrBkrsdnGWsgjLEyJ4"
     };
+  }
+
+
+  //Connection to blockchain
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert("Please use metamask !")
+    }
   }
 
 
@@ -35,7 +90,7 @@ class App extends Component {
     //2.Convert file to buffer to upload/send to ipfs
     reader.readAsArrayBuffer(file)
     reader.onloadend = () => {
-      this.setState({buffer: Buffer(reader.result)})
+      this.setState({ buffer: Buffer(reader.result) })
       console.log('buffer', Buffer(reader.result))
     }
   }
@@ -43,7 +98,7 @@ class App extends Component {
   captureType = (event) => {
     event.preventDefault()
     const type = document.getElementById('type').value
-    this.setState({type : type})
+    this.setState({ type: type })
     console.log(type)
   }
 
@@ -54,25 +109,41 @@ class App extends Component {
 
     //upload file to ipfs
     ipfs.add(this.state.buffer, (error, result) => {
-    console.log('ipfs result', result)
+      console.log('ipfs result', result)
 
       //getting hash of file from ipfs
       const fileHash = result[0].hash
       console.log(fileHash)
-      if(error){
+      if (error) {
         console.error(error)
         return
       }
 
-      //store hash on blockchain
-      // this.state.contract.methods.setInfo(this.state.type, fileHash, 0).send({from: this.state.account}).then((r) => {
-      //   this.setState({fileHash: fileHash})
-      // })
+      // store hash on blockchain
+      this.state.contract.methods.setInfo(this.state.type, fileHash, 0).send({ from: this.state.account }).then((r) => {
+        this.setState({ fileHash: fileHash })
+      })
     })
 
-    
+
   }
 
+  showFile = (event) => {
+     var f1 = document.getElementById('f1')
+     if(this.state.record[0]!=='null')f1.style.display = 'block';
+
+     var f2 = document.getElementById('f2')
+     if(this.state.record[1]!=='null')f2.style.display = 'block';
+
+     var f3 = document.getElementById('f3')
+     if(this.state.record[2]!=='null')f3.style.display = 'block';
+
+     var f4 = document.getElementById('f4')
+     if(this.state.record[3]!=='null')f4.style.display = 'block';
+
+     var f5 = document.getElementById('f5')
+     if(this.state.record[4]!=='null')f5.style.display = 'block';
+  }
 
   render() {
     return (
@@ -86,6 +157,14 @@ class App extends Component {
           >
             PISB
           </a>
+
+          {/* display the account connected with metamask */}
+          <ul className='navbar-nav px-3'>
+            <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
+              <small className='text-white'>{this.state.account}</small>
+            </li>
+          </ul>
+
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
@@ -100,26 +179,35 @@ class App extends Component {
                 </a>
                 <h1>National Institute Of Technology Patna</h1>
                 <h4>
-                 Personal Information Storage Using Etherium Blockchain
+                  Personal Information Storage Using Etherium Blockchain
                 </h4>
 
                 <div className="content">
                   <h3>Add Your Files</h3>
                   <form action="" className='form' onSubmit={this.onSubmit}>
-                    <input type="file" onChange={this.captureFile}/>
+                    <input type="file" onChange={this.captureFile} />
                     <select className='mr-5' name="type" id="type" onChange={this.captureType}>
-                       <option value="aadhar">Aadhar Card</option>
-                       <option value="drive">Driving License</option>
-                       <option value="pan">Pan Card</option>
-                       <option value="tenth">Tenth Certificate</option>
-                       <option value="twelth">Twelth Certificate</option>
+                      <option value="aadhar">Aadhar Card</option>
+                      <option value="drive">Driving License</option>
+                      <option value="pan">Pan Card</option>
+                      <option value="tenth">Tenth Certificate</option>
+                      <option value="twelth">Twelth Certificate</option>
                     </select>
-                    
+
                     <input type="submit" />
                   </form>
-          
+
+                  {/* Files  */}
                   <div className="mt-4">
-                    <a className='fileshow'  href={`https://ipfs.infura.io/ipfs/${this.state.fileHash}`}>Get file</a>
+                    <a id='f1' className='fileshow mx-1' href={`https://ipfs.infura.io/ipfs/${this.state.record[0]}`}>Show file</a>
+                    <a id='f2' className='fileshow mx-1' href={`https://ipfs.infura.io/ipfs/${this.state.record[1]}`}>Show file</a>
+                    <a id='f3' className='fileshow mx-1' href={`https://ipfs.infura.io/ipfs/${this.state.record[2]}`}>Show file</a>
+                    <a id='f4' className='fileshow mx-1' href={`https://ipfs.infura.io/ipfs/${this.state.record[3]}`}>Show file</a>
+                    <a id='f5' className='fileshow mx-1' href={`https://ipfs.infura.io/ipfs/${this.state.record[4]}`}>Show file</a>
+                  </div>
+
+                  <div className='mt-2'>
+                    <button onClick={this.showFile}>Show File</button>
                   </div>
 
                 </div>
